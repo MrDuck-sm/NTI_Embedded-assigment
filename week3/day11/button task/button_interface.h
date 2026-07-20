6eb5974c91b377f65e8d260713de97b3c6ec44f4
MrDuck-sm <salmafouadothman@gmail.com>
2026-07-20 21:54:05 +0300

    Add files via upload

---
+++ week3/day11/button task/button_interface.h
@@ -0,0 +1,9 @@
+#ifndef _BUTTON_INTERFACE_H
+#define _BUTTON_INTERFACE_H
+
+#include<stdio.h>
+#include<stdint.h>
+#include"../../../../week2/day10/Atmega32RegMap.h"
+#include"../../../../week2/day8/bitmath/bitmath.h"
+
+#endif
