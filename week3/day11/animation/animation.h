cdf3ceb1f1e647a4a53bd2d18883350ab5bc00c4
MrDuck-sm <salmafouadothman@gmail.com>
2026-07-20 20:33:59 +0300

    Add files via upload

---
+++ week3/day11/animation/animation.h
@@ -0,0 +1,9 @@
+#ifndef _ANIMATION_H
+#define _ANIMATION_H
+
+#include<stdio.h>
+#include<stdint.h>
+#include"../../../../week2/day10/Atmega32RegMap.h"
+#include <util/delay.h>
+#include"../../../../week2/day8/bitmath/bitmath.h"
+#endif
